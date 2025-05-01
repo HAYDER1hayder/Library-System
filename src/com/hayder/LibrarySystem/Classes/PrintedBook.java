f6587ae1cb7643f0ae8b4daa27358a79f224c6ca
HAYDER1hayder <hayderkays30@gmail.com>
2025-05-01 15:03:35 +0100

    Update PrintedBook.java

--- src/com/hayder/LibrarySystem/Classes/PrintedBook.java
+++ src/com/hayder/LibrarySystem/Classes/PrintedBook.java
@@ -1,10 +1,11 @@
 package com.hayder.LibrarySystem.Classes;
 public class PrintedBook extends Book {
     public PrintedBook(String title, String author, String isbn) {
+        //Constructors avec parameter inheritance Book
         super(title, author, isbn);
     }
 
-    @Override
+    // Override
     public String getBookType() {
         return "Printed";
     }
