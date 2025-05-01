7ca147f1fcf29a8247942d2149b95a5558c6049e
HAYDER1hayder <hayderkays30@gmail.com>
2025-05-01 15:01:37 +0100

    Update EBook.java

--- src/com/hayder/LibrarySystem/Classes/EBook.java
+++ src/com/hayder/LibrarySystem/Classes/EBook.java
@@ -2,10 +2,11 @@ package com.hayder.LibrarySystem.Classes;
 
 public class EBook extends Book {
     public EBook(String title, String author, String isbn) {
+        // Constructors avec parameter inheritance Book
         super(title, author, isbn);
     }
 
-    @Override
+    // Override
     public String getBookType() {
         return "Electronic";
     }
