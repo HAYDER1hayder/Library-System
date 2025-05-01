a8cb1775f53bd73b2c165110453a715c1edd39e8
HAYDER1hayder <hayderkays30@gmail.com>
2025-05-01 14:57:02 +0100

    Update Borrower.java

--- src/com/hayder/LibrarySystem/Classes/Borrower.java
+++ src/com/hayder/LibrarySystem/Classes/Borrower.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Borrower{
-    private String name;
+    private String name;// Attribute _ Encapsulation 
     private String studentId;
     private List<Book> borrowedBooks;
-
-    public Borrower(String name, String studentId) {
+    
+    // Constructors avec parameter
+    public Borrower(String name, String studentId) { 
         this.name = name;
         this.studentId = studentId;
         this.borrowedBooks = new ArrayList<>();
@@ -25,7 +26,7 @@ public class Borrower{
         borrowedBooks.remove(book);
     }
 
-    public List<Book> getBorrowedBooks() {
+    public List<Book> getBorrowedBooks() {// Function of type List<Book> 
         return borrowedBooks;
     }
 }
